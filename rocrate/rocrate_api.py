57a10c1ba555e65f5e46f28e9c82c9c977c3e736
ieguinoa <ignacio.eguinoa@gmail.com>
2020-05-13 10:28:24 +0200

    move galaxy2cwl to api

--- rocrate/rocrate_api.py
+++ rocrate/rocrate_api.py
@@ -1,10 +1,29 @@
 import rocrate.rocrate as roc
-
+from galaxy2cwl import get_cwl_interface
 
 # Exposed functions
 
-def make_workflow_rocrate(workflow_path,wf_type,incl_files=[],cwl_abstract=None,diagram=None):
-    #returns a complete ROCrate object
-    wf_crate = roc.ROCrateWorkflow(main_workflow_path,wf_type)
+def make_workflow_rocrate(workflow_path,wf_type,include_files=[],cwl=None,diagram=None):
+
+    #returns a complete ROCrate object corresponding to a Workflow template file
+    # wf_type: Galaxy, CWL , Nextflow..
+    # cwl: CWL/CWL-Abstract representation of the workflow. If the 
+    # diagram: an image/graphical workflow representation. 
+    #         If a CWL/CWLAbstract file is provided then this is generated using cwltool
+
+    wf_crate = roc.ROCrateWorkflow(workflow_path,wf_type)
+
+    if wf_type != 'CWL':
+        if cwl:
+            #add cwl file to crate
+            print('add cwl file')
+        elif wf_type == 'Galaxy':
+            #create cwl_abstract
+            cwl_abstract = get_cwl_interface.main(['1',workflow_file])
+            cwl_abstract_path = tempfile.TemporaryFile(mode="w")
+            json.dump(cwl_abstract, cwl_abstract_path)
+            # fix the dest_path of this
+            self.add_file(cwl_abstract_path)
     return wf_crate
+
 
