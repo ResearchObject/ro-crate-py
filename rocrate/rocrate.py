1f4653b38a2ba0e68a90163f7cd37d65b758ad22
ieguinoa <ignacio.eguinoa@gmail.com>
2020-06-09 18:36:02 +0200

    some general work

--- rocrate/rocrate.py
+++ rocrate/rocrate.py
@@ -1,14 +1,19 @@
 import os
 import uuid
+# from uuid import UUID
 from pathlib import Path
 
 from .model import contextentity
 from .model.root_dataset import RootDataset
 from .model.file import File
+from .model.person import Person
 from .model.dataset import Dataset
 from .model.metadata import Metadata
 import zipfile
 import tempfile
+
+from arcp import generate
+from arcp import arcp_random
 
 class ROCrate():
 
@@ -16,7 +21,7 @@ class ROCrate():
         self.default_entities = []
         self.data_entities = []
         self.contextual_entities = []
-        self.uuid = uuid.uuid1()
+        self.uuid = uuid.uuid4()
 
         #create metadata and assign it to data_entities
         self.metadata = Metadata(self)  # metadata init already includes itself into the root metadata
@@ -31,29 +36,48 @@ class ROCrate():
         #self.preview = Preview('ro-crate-preview.html')
         #self.default_entities.append(self.preview)
 
-    def resolve_id(relative_id):
-        os.path.join(("arcp://uuid," + self.uuid), relative_id)
+
+    # Properties
+
+    @property
+    def author(self):
+        return self.root_dataset['author']
+
+    @author.setter
+    def author(self, value):
+        # if isinstance(value, Person):
+        self.root_dataset['author'] = value
+
+
+    def resolve_id(self, relative_id):
+        return generate.arcp_random(relative_id.strip('./'), uuid=self.uuid)
 
     def get_entities(self):
         return self.default_entities + self.data_entities + self.contextual_entities
 
     def set_main_entity(self, main_entity):
-        self.root_dataset.setitem('mainEntity', main_entity)
+        self.root_dataset['mainEntity'] = main_entity
 
     def _get_root_jsonld(self):
         self.root_dataset.properties()
-        # root_graph = self.metadata.get
-        #root_json_ld self._get_root_jsonld() .serialize(format='json-ld', indent=4)
+
+    def dereference(self, entity_id):
+        canonical_id = self.resolve_id(entity_id)
+        for entity in self.get_entities():
+            if canonical_id == entity.canonical_id():
+                return entity
+        return None
 
     # source: file object or path (str)
     def add_file(self, source, crate_path = None , properties = None):
+        # print('adding file' + source)
         file_entity = File(self, source,crate_path,properties)
         self._add_data_entity(file_entity)
         return file_entity
 
     def remove_file(self,file_id):
         #if file in data_entities:
-        _remove_data_entity(file_id)
+        self._remove_data_entity(file_id)
 
     def add_directory(self, source, crate_path = None , properties = None):
         dataset_entity = Dataset(source,crate_path,properties)
@@ -61,7 +85,7 @@ class ROCrate():
 
     def remove_directory(self,dir_id):
         #if file in data_entities:
-        _remove_data_entity(dir_id)
+        self._remove_data_entity(dir_id)
 
     def _add_data_entity(self, data_entity):
         self._remove_data_entity(data_entity)
@@ -71,11 +95,31 @@ class ROCrate():
         if data_entity in self.data_entities:
             self.data_entities.remove(data_entity)
 
+
+
+    ################################
+    ##### Contextual entities ######
+    ################################
+
+    def _add_context_entity(self, entity):
+        if entity in self.contextual_entities: self.contextual_entities.remove(entity)
+        self.contextual_entities.append(entity)
+
+    def add_person(self, identifier, properties = {}):
+        new_person = Person(self, identifier,properties)
+        self._add_context_entity(new_person)
+        return new_person
+
+    #TODO
+    #def fetch_all(self):
+        # fetch all files defined in the crate
+
+    ################################
+    #####  ######
+    ################################
+
     # write crate to local dir
     def write_crate(self, base_path):
-        # write default entities (metadata, preview, root Dataset..)
-        #for default_entity in self.default_entities:
-        #    default_entity.write(base_path)
         Path(base_path).mkdir(parents=True, exist_ok=True)
         # write data entities
         for writable_entity in self.data_entities + self.default_entities:
@@ -87,20 +131,6 @@ class ROCrate():
         zf.close()
         archive = zf.filename
         return archive
-
-
-    ################################
-    ##### Contextual entities ######
-    ################################
-
-    def add_person(person_dict):
-        new_person = roc.Person()
-        _add_context_entity(new_person)
-
-    #TODO
-    #def fetch_all(self):
-        # fetch all files defined in the crate
-
 
 # class ROCrateWorkflow(ROCrate):
 
