a4894808e957e1078cf07dee4d0b5ce558631d76
ieguinoa <ignacio.eguinoa@gmail.com>
2020-06-18 19:39:49 +0200

    cleanup

--- rocrate/model/file.py
+++ rocrate/model/file.py
@@ -16,37 +16,22 @@
 
 import os
 
-from .data_entity import DataEntity
 from shutil import copy
 import pathlib
 
+from .data_entity import DataEntity
+
 class File(DataEntity):
 
-    def __init__(self, crate, source = None, dest_path = None , properties = None):
+    def __init__(self, crate, source=None, dest_path=None, properties=None):
         #...process source
-
-        #this first case was aimed at handling File objects but dont think its necessary, only allowing a path is ok.
-        # if isinstance(source, io.IOBase):
-            # # define destination path
-            # if not dest_path:  #no name for path within the crate
-                # dest = source.toString()
-            # else:
-                # dest = dest_path
-            # #copy to dest by chunks
         if dest_path:
             identifier = dest_path  # relative path?
         else:
             identifier = os.path.basename(source)
         if source and os.path.isfile(source):
             self.source = source
-        #else:
-            #print('source is' + source)
-            #print('The source is not a File not a correct path')
-            #return None
         super(File, self).__init__(crate, identifier, properties)
-
-    # def format_id(identifier):
-        # return identifier.strip('./')
 
     def _empty(self):
         val = {
@@ -60,7 +45,7 @@ class File(DataEntity):
         out_dir = pathlib.Path(os.path.dirname(out_file_path))
         if not out_dir.exists():
             os.mkdir(out_dir)
-        copy(self.source,out_file_path)
+        copy(self.source, out_file_path)
 
-    def write_zip(self,zip_out):
-        zip_out.write(self.source,self.id)
+    def write_zip(self, zip_out):
+        zip_out.write(self.source, self.id)
