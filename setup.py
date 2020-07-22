d4c496b4eefb6b4af41eb76941af63dcd466cfea
bedroesb <bedro@psb.vib-ugent.be>
2020-07-22 12:49:48 +0200

    readme.md

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
   long_description = f.read()
 
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
