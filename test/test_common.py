5b5742e0f114e8792f3b30add3b5a86b89a86c42
simleo <simleo@crs4.it>
2020-07-22 17:01:13 +0200

    test dir is now PEP8-compliant

--- test/test_common.py
+++ test/test_common.py
@@ -1,9 +1,7 @@
 import os
-import sys
 import shutil
 import tempfile
 import unittest
-
 
 
 class BaseTest(unittest.TestCase):
@@ -11,8 +9,11 @@ class BaseTest(unittest.TestCase):
     def setUp(self):
         self.tmpdir = tempfile.mkdtemp(prefix="rocrate_test_")
 
-        #copy test data
-        shutil.copytree(os.path.abspath(os.path.join('test', 'test-data')), os.path.join(self.tmpdir,'test-data'))
+        # copy test data
+        shutil.copytree(
+            os.path.abspath(os.path.join('test', 'test-data')),
+            os.path.join(self.tmpdir, 'test-data')
+        )
         self.test_data_dir = os.path.join(self.tmpdir, 'test-data')
         self.assertTrue(os.path.isdir(self.test_data_dir))
 
