c5ec92f253672770cc8324aa52babe880e55838f
simleo <simleo@crs4.it>
2020-07-22 16:12:32 +0200

    cleanup: test_model

--- test/test_model.py
+++ test/test_model.py
@@ -1,12 +1,11 @@
-import unittest
 import os
 from rocrate.rocrate import ROCrate
 from rocrate.model.file import File
 from rocrate.model.person import Person
 from test.test_common import BaseTest
 
-class TestAPI(BaseTest):
 
+class TestAPI(BaseTest):
 
     def test_dereferencing(self):
         crate = ROCrate()
@@ -23,7 +22,7 @@ class TestAPI(BaseTest):
         self.assertIsInstance(file_returned, File)
         dereference_file = crate.dereference("sample_file.txt")
         self.assertIsInstance(dereference_file, File)
-        self.assertEqual(file_returned,dereference_file)
+        self.assertEqual(file_returned, dereference_file)
 
     def test_dereferencing_equivalent_id(self):
         crate = ROCrate()
@@ -40,24 +39,22 @@ class TestAPI(BaseTest):
 
     def test_contextual_entities(self):
         crate = ROCrate()
-        new_person = crate.add_person('joe' , {'name': 'Joe Pesci'})
+        new_person = crate.add_person('joe', {'name': 'Joe Pesci'})
         person_dereference = crate.dereference('#joe')
         person_prop = person_dereference.properties()
         self.assertEqual(person_prop['@type'], 'Person')
         self.assertEqual(person_prop['name'], 'Joe Pesci')
+        self.assertEqual(new_person.properties(), person_prop)
 
     def test_properties(self):
         crate = ROCrate()
 
-        new_person = crate.add_person('001' , {'name': 'Lee Ritenour'})
+        new_person = crate.add_person('001', {'name': 'Lee Ritenour'})
         crate.creator = new_person
         self.assertIsInstance(crate.creator, Person)
         self.assertEqual(crate.creator['name'], 'Lee Ritenour')
 
-        new_person2 = crate.add_person('002' , {'name': 'Lee Ritenour'})
+        new_person2 = crate.add_person('002', {'name': 'Lee Ritenour'})
 
-        crate.creator = [new_person,new_person2]
-        self.assertIsInstance(crate.creator,list)
-
-
-
+        crate.creator = [new_person, new_person2]
+        self.assertIsInstance(crate.creator, list)
