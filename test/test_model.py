4b77e54f3949625611e94069147ea3e6aaf8c7c0
Stian Soiland-Reyes <stain@apache.org>
2020-07-23 16:16:55 +0100

    test person id is # and UUIDv4

--- test/test_model.py
+++ test/test_model.py
@@ -2,7 +2,7 @@ from rocrate.rocrate import ROCrate
 from rocrate.model.file import File
 from rocrate.model.person import Person
 from test.test_common import BaseTest
-
+import uuid
 
 class TestAPI(BaseTest):
 
@@ -38,7 +38,7 @@ class TestAPI(BaseTest):
 
     def test_contextual_entities(self):
         crate = ROCrate()
-        new_person = crate.add_person('joe', {'name': 'Joe Pesci'})
+        new_person = crate.add_person('#joe', {'name': 'Joe Pesci'})
         person_dereference = crate.dereference('#joe')
         person_prop = person_dereference.properties()
         self.assertEqual(person_prop['@type'], 'Person')
@@ -48,12 +48,23 @@ class TestAPI(BaseTest):
     def test_properties(self):
         crate = ROCrate()
 
-        new_person = crate.add_person('001', {'name': 'Lee Ritenour'})
+        new_person = crate.add_person('#001', {'name': 'Lee Ritenour'})
         crate.creator = new_person
         self.assertIsInstance(crate.creator, Person)
         self.assertEqual(crate.creator['name'], 'Lee Ritenour')
 
-        new_person2 = crate.add_person('002', {'name': 'Lee Ritenour'})
+        new_person2 = crate.add_person('#002', {'name': 'Lee Ritenour'})
 
         crate.creator = [new_person, new_person2]
         self.assertIsInstance(crate.creator, list)
+
+    def test_uuid(self):
+        crate = ROCrate()
+        new_person = crate.add_person(name="No Identifier")
+        jsonld = new_person.as_jsonld()
+        self.assertEqual("Person", jsonld["@type"])
+        self.assertTrue(jsonld["@id"].startswith("#"))
+        # Check it made a valid UUIDv4
+        u = uuid.UUID(jsonld["@id"][1:])
+        self.assertEqual(4, u.version)
+    
