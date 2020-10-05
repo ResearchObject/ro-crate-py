d62eeb11726699ba33625d3155270f2017e10ece
simleo <simleo@crs4.it>
2020-10-05 16:35:04 +0200

    expand test_read

--- test/test_read.py
+++ test/test_read.py
@@ -1,25 +1,80 @@
 from rocrate.rocrate import ROCrate
 
+_URL = ('https://raw.githubusercontent.com/ResearchObject/ro-crate-py/master/'
+        'test/test-data/sample_file.txt')
 
-def test_crate_dir_loading(test_data_dir, tmpdir):
+
+def test_crate_dir_loading(test_data_dir, tmpdir, helpers):
     # load crate from directory
     crate_dir = test_data_dir / 'read_crate'
     crate = ROCrate(crate_dir, load_preview=True)
 
     # check loaded entities and properties
+    root = crate.dereference('./')
+    assert crate.root_dataset is root
+    root_prop = root.properties()
+    assert root_prop['@id'] == root.id
+    assert root_prop['@id'] == './'
+    assert root_prop['@type'] == 'Dataset'
+
+    metadata = crate.dereference(helpers.METADATA_FILE_NAME)
+    assert crate.metadata is metadata
+    md_prop = metadata.properties()
+    assert md_prop['@id'] == metadata.id
+    assert md_prop['@id'] == helpers.METADATA_FILE_NAME
+    assert md_prop['@type'] == 'CreativeWork'
+    assert md_prop['about'] == {'@id': './'}
+    assert md_prop['conformsTo'] == {'@id': 'https://w3id.org/ro/crate/1.0'}
+    assert metadata.root is root
+
+    preview = crate.dereference('ro-crate-preview.html')
+    assert preview == crate.preview
+    preview_prop = preview.properties()
+    assert preview_prop['@id'] == 'ro-crate-preview.html'
+    assert preview_prop['@id'] == preview.id
+    assert preview_prop['@type'] == 'CreativeWork'
+    assert preview_prop['about'] == {'@id': './'}
+
     main_wf = crate.dereference('test_galaxy_wf.ga')
     wf_prop = main_wf.properties()
     assert wf_prop['@id'] == 'test_galaxy_wf.ga'
+    assert wf_prop['@id'] == main_wf.id
+    # assert set(wf_prop['@type']) == helpers.WORKFLOW_TYPES
+    # assert wf_prop['@programmingLanguage'] == {"@id": "https://galaxyproject.org"}
+    # assert wf_prop["subjectOf"] == {"@id": "abstract_wf.cwl"}
+
+    abs_wf = crate.dereference('abstract_wf.cwl')
+    abs_wf_prop = abs_wf.properties()
+    assert abs_wf_prop['@id'] == 'abstract_wf.cwl'
+    assert abs_wf_prop['@id'] == abs_wf.id
+    # assert set(abs_wf_prop['@type']) == helpers.WORKFLOW_TYPES
 
     wf_author = crate.dereference('#joe')
     author_prop = wf_author.properties()
+    assert author_prop['@id'] == '#joe'
+    assert author_prop['@id'] == wf_author.id
     assert author_prop['@type'] == 'Person'
     assert author_prop['name'] == 'Joe Bloggs'
+
+    test_file = crate.dereference('test_file_galaxy.txt')
+    test_file_prop = test_file.properties()
+    assert test_file_prop['@id'] == 'test_file_galaxy.txt'
+    assert test_file_prop['@id'] == test_file.id
+
+    remote_file = crate.dereference(_URL)
+    remote_file_prop = remote_file.properties()
+    assert remote_file_prop['@id'] == _URL
+    assert remote_file_prop['@id'] == remote_file.id
+
+    dataset = crate.dereference('examples/')
+    dataset_prop = dataset.properties()
+    # assert dataset_prop['@id'] == 'examples/'
+    assert dataset_prop['@id'] == dataset.id
 
     # write the crate in a different directory
     out_path = tmpdir / 'crate_read_out'
     out_path.mkdir()
     crate.write_crate(out_path)
 
-    metadata_path = out_path / 'ro-crate-metadata.jsonld'
+    metadata_path = out_path / helpers.METADATA_FILE_NAME
     assert metadata_path.exists()
