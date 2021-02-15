8df91951a63eadb4f2816097b1923137a6f36be9
simleo <simleo@crs4.it>
2021-02-15 16:53:26 +0100

    add test_cli

---
+++ test/test_cli.py
@@ -0,0 +1,31 @@
+# Copyright 2019-2020 The University of Manchester, UK
+# Copyright 2020 Vlaams Instituut voor Biotechnologie (VIB), BE
+# Copyright 2020 Barcelona Supercomputing Center (BSC), ES
+# Copyright 2020 Center for Advanced Studies, Research and Development in Sardinia (CRS4), IT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from click.testing import CliRunner
+
+from rocrate.cli import cli
+
+
+def test_cli_init(test_data_dir, tmpdir, helpers):
+    crate_dir = test_data_dir / "ro-crate-galaxy-sortchangecase"
+    metadata_path = crate_dir / helpers.METADATA_FILE_NAME
+    metadata_path.unlink()
+
+    runner = CliRunner()
+    result = runner.invoke(cli, ["init", str(crate_dir)])
+    assert result.exit_code == 0
+    assert metadata_path.is_file()
