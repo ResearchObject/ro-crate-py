419670d42ade081e94a08721ec5a43058286c31c
simleo <simleo@crs4.it>
2024-12-10 10:02:38 +0100

    fix for windows

--- rocrate/model/file_or_dir.py
+++ rocrate/model/file_or_dir.py
@@ -47,5 +47,5 @@ class FileOrDir(DataEntity):
             if is_url(str(source)):
                 identifier = os.path.basename(source) if fetch_remote else source
             else:
-                identifier = os.path.basename(str(source).rstrip(os.sep))
+                identifier = os.path.basename(str(source).rstrip("/"))
         super().__init__(crate, identifier, properties)
