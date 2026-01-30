26a207183fdf0c95554fec900bfc4dfd0c04b01a
simleo <simleo@crs4.it>
2026-01-30 16:42:10 +0100

    add test_from_uri_detached

--- test/test_read.py
+++ test/test_read.py
@@ -945,3 +945,29 @@ def test_from_uri(tmpdir):
     assert (out_path / "ro-crate-preview.html").is_file()
     assert (out_path / test_fn).is_file()
     assert (out_path / "sample_file.txt").is_file()
+
+
+@pytest.mark.filterwarnings("ignore")
+def test_from_uri_detached(tmpdir):
+    source = ("https://raw.githubusercontent.com/ResearchObject/ro-crate-py/"
+              "detached_test_data/test/test-data/detached-ro-crate-metadata.json")
+    base_uri = ("https://raw.githubusercontent.com/ResearchObject/ro-crate-py/"
+                "detached_test_data/test/test-data/")
+    crate = ROCrate(source)
+    assert crate.source == source
+    assert crate.root_dataset.id == base_uri
+    sample_file = crate.get(f"{base_uri}sample_file.txt")
+    assert sample_file.source == f"{base_uri}sample_file.txt"
+    test_file_galaxy = crate.get(f"{base_uri}test_file_galaxy.txt")
+    assert test_file_galaxy.source == f"{base_uri}test_file_galaxy.txt"
+    assert test_file_galaxy.get("localPath") == "test-data/test_file_galaxy.txt"
+    out_path = tmpdir / "out_crate"
+    crate.write(out_path)
+    assert (out_path / "ro-crate-metadata.json").is_file()
+    assert not (out_path / "sample_file.txt").exists()
+    assert not (out_path / "test_file_galaxy.txt").exists()
+    assert not (out_path / "test-data" / "test_file_galaxy.txt").exists()
+    test_file_galaxy.fetch_remote = True
+    shutil.rmtree(out_path)
+    crate.write(out_path)
+    assert (out_path / "test-data" / "test_file_galaxy.txt").exists()
